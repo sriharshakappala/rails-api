14978c63bbb4d87c45addefc7928fe75f208b2df
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2014-11-21 12:09:22 +0530

    Lets start breaking things

--- learn3/db/schema.rb
+++ learn3/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141121055636) do
+ActiveRecord::Schema.define(version: 20141121063557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20141121055636) do
     t.string   "name"
     t.integer  "price"
     t.integer  "category_id"
-    t.date     "released_on"
+    t.datetime "released_at"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
